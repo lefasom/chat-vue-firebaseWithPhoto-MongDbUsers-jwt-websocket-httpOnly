a01db17a0005b5a3180d31b6b687304ce3ff84c8
lefasom <leosombra89@gmail.com>
2023-09-16 16:22:16 -0300

    cambio uta de backend

--- frontend/src/vuex/store.js
+++ frontend/src/vuex/store.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 import { db } from '../firebase/firebase'
 import { getDocs, collection, doc, deleteDoc, addDoc, updateDoc, getDoc, query, orderBy, onSnapshot } from 'firebase/firestore'
 import Axios from 'axios'
-Axios.defaults.baseURL = "http://localhost:3001"
+Axios.defaults.baseURL = "https://chatea.onrender.com"
 
 const store = createStore({
   state() {
